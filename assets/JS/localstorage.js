7b0189f6ef3c10e98aef0525959efe66df243c86
LisaMLorenz <hej@frau-lorenz.de>
2023-03-24 09:19:36 +0000

    if textfield is changed button changes back to save

--- assets/JS/localstorage.js
+++ assets/JS/localstorage.js
@@ -9,54 +9,75 @@ $(".saveBtn").on("click", function () {
     // Get the hour ID
     var hour = $(this).prev(".description").attr("id");
 
-// Check if the input value is empty
-if (inputValue === "") {
-    console.log("Input value is empty.");
-    return; // Stop the function execution
-}
+    // Check if the input value is empty
+    if (inputValue === "") {
+        console.log("Input value is empty.");
+        return; // Stop the function execution
+    }
 
     // Save the input value to local storage
-localStorage.setItem(hour, inputValue);
+    localStorage.setItem(hour, inputValue);
 
-// Change the button text and color
+    // Change the button text and color
+    if ($(this).text() === "Complete") {
+        $(this).text("Save");
+        $(this).css("background-color", "#06aed5")
+    } else {
+        $(this).text("Done?");
+        $(this).css("background-color", "violet")
 
-if ($(this).text() === "Delete") {
-    $(this).text("Save");
-    $(this).css("background-color", "#06aed5")
-} else {
-    $(this).text("Delete");
-    $(this).css("background-color", "red")
-}
+    }
 
+    const saveButton = $(this);
 
-const saveButton = $(this);
+    var savedButtonValue = localStorage.getItem(hour + "-button");
+    if (savedButtonValue !== null) {
+        saveButton.text(savedButtonValue);
+    }
 
-var savedButtonValue = localStorage.getItem(hour + "-button");
-if (savedButtonValue !== null) {
-    saveButton.text(savedButtonValue);
-}
+    // Check if the button text content is "Done?" and change the background color accordingly
+    if (saveButton.textContent === "Done?") {
+        $(saveButton).css("background-color", "violet");
+    } else {
+        saveButton.textContent = "Save";
+    }
 
-// Check if the button text content is "Delete" and change the background color accordingly
-if (saveButton.textContent === "Delete") {
-    $(saveButton).css("background-color", "red");
-} else {
-    saveButton.textContent = "Save";
-    
-}
+    saveButton.textContent = "Done?";
 
-saveButton.textContent = "Delete";
-
-console.log("Save button clicked.");
+    console.log("Save button clicked.");
 
     // unbind the original click event
     $(this).unbind("click");
 
     // bind a new click event with a different function
     $(this).click(function () {
-        // perform the new function here
-        console.log("Button functionality changed!");
+        if ($(this).text() === "Done?") {
+          $(this).text("Done!");
+          $(this).css("background-color", "green");
+        } else {
+          $(this).text("Done?");
+        }
+      });
+});
 
-    });
+// Add event listener to the text field to listen for any change in its value
+$(".description").on("input", function () {
+    // Get the value of the text field
+    var inputValue = $(this).val();
+    // Get the corresponding button
+    var saveButton = $(this).next(".saveBtn");
+
+    // Check if the input value is empty
+    if (inputValue === "") {
+        saveButton.text("Save");
+        saveButton.css("background-color", "#06aed5");
+    } else {
+        // Change the button text and color back to its original state
+        if (saveButton.text() === "Done?") {
+            saveButton.text("Save");
+            saveButton.css("background-color", "#06aed5");
+        }
+    }
 });
 
 // Retrieve the saved values from local storage
@@ -65,9 +86,8 @@ $(".description").each(function () {
     var savedValue = localStorage.getItem(hour);
     if (savedValue !== null) {
         $(this).val(savedValue);
-        $(this).next("#saveBtn").text("Delete");
-        $(this).next('#saveBtn').css("background-color", "red");
-        localStorage.setItem(hour + "-button", "Delete");
+        $(this).next("#saveBtn").text("Done?");
+        $(this).next('#saveBtn').css("background-color", "violet");
+        localStorage.setItem(hour + "-button", "Done?");
     }
 });
-
