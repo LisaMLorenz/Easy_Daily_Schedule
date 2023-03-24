09110f688214baace7ece2f4b2072bd7268bdaf4
LisaMLorenz <hej@frau-lorenz.de>
2023-03-24 08:50:10 +0000

    save button adds to local storage

--- assets/JS/localstorage.js
+++ assets/JS/localstorage.js
@@ -1,24 +1,73 @@
 let userInput = document.getElementsByClassName('description');
 let dailyTasks = []; // empty array 
+let saveButton = document.getElementById("saveBtn");
 
-$("saveBtn").click(function () { //Function to store input with save button
+// Define the click event listener for the save button
+$(".saveBtn").on("click", function () {
+    // Get the input value
+    var inputValue = $(this).prev(".description").val();
+    // Get the hour ID
+    var hour = $(this).prev(".description").attr("id");
 
-    const addTaskToLocalStorage = (ev) => {
-        ev.preventDefault();
-    }
-
-
-    localStorage.setItem("task", userInput.val());
-    dailyTasks.push(userInput);
-
-    var taskInPut = document.querySelector('description');
-    taskInPut.textContent = "\n" + JSON.stringify(dailyTasks, '\t', 2);
-
+// Check if the input value is empty
+if (inputValue === "") {
+    console.log("Input value is empty.");
+    return; // Stop the function execution
 }
-);
 
-$('saveBtn').click(function () {
-    dailyTasks.push(dailyTasks);
-    localStorage.setItem('tasks', JSON.stringify(dailyTasks))
-})
+    // Save the input value to local storage
+localStorage.setItem(hour, inputValue);
+
+// Change the button text and color
+
+if ($(this).text() === "Delete") {
+    $(this).text("Save");
+    $(this).css("background-color", "#06aed5")
+} else {
+    $(this).text("Delete");
+    $(this).css("background-color", "red")
+}
+
+
+const saveButton = $(this);
+
+var savedButtonValue = localStorage.getItem(hour + "-button");
+if (savedButtonValue !== null) {
+    saveButton.text(savedButtonValue);
+}
+
+// Check if the button text content is "Delete" and change the background color accordingly
+if (saveButton.textContent === "Delete") {
+    $(saveButton).css("background-color", "red");
+} else {
+    saveButton.textContent = "Save";
+    
+}
+
+saveButton.textContent = "Delete";
+
+console.log("Save button clicked.");
+
+    // unbind the original click event
+    $(this).unbind("click");
+
+    // bind a new click event with a different function
+    $(this).click(function () {
+        // perform the new function here
+        console.log("Button functionality changed!");
+
+    });
+});
+
+// Retrieve the saved values from local storage
+$(".description").each(function () {
+    var hour = $(this).attr("id");
+    var savedValue = localStorage.getItem(hour);
+    if (savedValue !== null) {
+        $(this).val(savedValue);
+        $(this).next("#saveBtn").text("Delete");
+        $(this).next('#saveBtn').css("background-color", "red");
+        localStorage.setItem(hour + "-button", "Delete");
+    }
+});
 
